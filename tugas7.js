701cb3835ac229737cd57cd8d843ca115edeb3eb
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-04-27 15:18:28 +0700

    Tugas 7 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas7.js
@@ -0,0 +1,13 @@
+var tinggiBadan = [170,160,165,177,187,199,205];
+
+for (let i = 0; i < tinggiBadan.length; i++) {
+    console.log(tinggiBadan[i]);
+   
+    
+}
+
+
+
+for (let x of tinggiBadan) {
+    console.log(x);
+}
